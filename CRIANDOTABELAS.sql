3f44e1b1b2b69fd1c7d81d24a99ba38694c09f4a
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2018-10-16 15:44:23 -0300

    Create CRIANDOTABELAS.sql

---
+++ CRIANDOTABELAS.sql
@@ -0,0 +1,24 @@
+CREATE TABLE TBL_FUNCIONARIO (
+	id_func int NOT NULL,
+	nome VARCHAR(30),
+	salario int,
+	id_depto int,
+	PRIMARY KEY (id_func),
+	);
+
+CREATE TABLE TBL_DEPTO (
+	id_depto int,
+	nome VARCHAR(30),
+	ramal int,
+	PRIMARY KEY (id_depto)
+);
+
+
+		  
+
+   
+
+
+
+	
+
